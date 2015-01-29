8a0a1c88212300e9c4d15bd487d7f9ca5e1a7a3f
Jessica <ladyjtm@gmail.com>
2015-01-28 18:55:32 -0800

    limit character input

--- spec/survey_spec.rb
+++ spec/survey_spec.rb
@@ -10,4 +10,9 @@ describe(Survey) do
     survey = Survey.create({:survey_name => "sleeping hours"})
     expect(survey.survey_name()).to(eq("Sleeping hours"))
   end
+
+  it("ensures the length of survey name is at most 30 characters") do
+    survey = Survey.new({:survey_name => "a".*(31)})
+    expect(survey.save()).to(eq(false))
+  end
 end
